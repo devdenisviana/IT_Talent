d7251a1d57c966a75ab9f446bf4b475ef02fd6a2
Denis Jeronimo Viana Filho <41395264+devdenisviana@users.noreply.github.com>
2024-02-19 14:56:40 -0300

    Introdução a java
    Conceitos básicos e tipos de dados

---
+++ Aulas/Java_introducao/src/Comentario.java
@@ -0,0 +1,14 @@
+/**
+ * JAVADOC - Comentário voltado a documentação do arquivo
+ */
+public class Comentario {
+    public static void main(String[] args) {
+        //comentario em linha
+        System.out.println();
+    }
+}
+
+/*
+* Comentario em blocos
+*
+* */
---
+++ Aulas/Java_introducao/src/Variaveis.java
@@ -0,0 +1,29 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Denis";
+        System.out.println("Meu nome é: "+nome);
+
+        System.out.println("Mínimo Valor Short: "+ Short.MIN_VALUE + " e Máximo valor Short: "+ Short.MAX_VALUE);
+        System.out.println("Mínimo Valor Integer: "+ Integer.MIN_VALUE + " e Máximo valor Integer: "+ Integer.MAX_VALUE);
+        System.out.println("Mínimo Valor Long: "+ Long.MIN_VALUE + " e Máximo valor Long: "+ Long.MAX_VALUE);
+        System.out.println("Mínimo Valor Byte: "+ Byte.MIN_VALUE + " e Máximo valor Byte: "+ Byte.MAX_VALUE);
+
+
+    }
+}
+
+/** Tipos de dados
+ * Tipo             Bit
+ * Double           64
+ * Float            32
+ *
+ * Long             64
+ * Integer          32
+ * Short            16
+ * Byte             8
+ *
+ *                  Depende do uso
+ * Boolean          ?
+ * String           ?
+ * Character        ?
+ * */
