62f8de94e54b8a047b8681764b7b2d331af8fc30
Denis Jeronimo Viana Filho <41395264+devdenisviana@users.noreply.github.com>
2024-02-24 09:42:39 -0300

    Operadores matematicos

--- Aulas/Java_introducao/src/Dados.java
+++ Aulas/Java_introducao/src/Dados.java
@@ -1,2 +1,28 @@
 public class Dados {
+    public static void main(String[] args) {
+        //tipos por referencia
+        Byte oByte = 5;
+        Short oShort = 5;
+        Integer oInteger = 5;
+        Long oLong = 10L;
+        Float oFloat = 10.0f;
+        Double oDouble = 10.0;
+        Boolean oBollean = true;
+        String str = "teste";
+        Character c = 'c';
+
+        //tipos primitivos
+        byte aByte = 5;
+        short aShort = 5;
+        int aInteger = 5;
+        long aLong = 10L;
+        float aFloat = 10.0f;
+        double aDouble = 10.0;
+        boolean aBollean = true;
+        char b = 'b';
+
+        System.out.println(oByte.toString());
+        System.out.println(aByte);
+
+    }
 }
---
+++ Aulas/Java_introducao/src/Operadores_Math.java
@@ -0,0 +1,14 @@
+public class Operadores_Math {
+    public static void main(String[] args) {
+        System.out.println("Adição => 5 + 2 = " + (5 + 2));
+        System.out.println("Subtração => 5 - 2 = " + (5 - 2));
+        System.out.println("Multiplicação => 5 * 2 = " + (5 * 2));
+        System.out.println("Quociente da divisão => 5 / 2 = " + (5 / 2));
+        System.out.println("Resto da divisão => 5 % 2 = "+ (5 % 2));
+        System.out.println("Para exibir o quociente exato da divisão: ");
+        float a = 5.0f;
+        float b = 2.0f;
+        System.out.println("O quociente da divisão de a / b = "+(a/b));
+
+    }
+}
