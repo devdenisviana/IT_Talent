9f7474c8bfdd132a3cea8288b263f899fabe5e76
Denis Jeronimo Viana Filho <41395264+devdenisviana@users.noreply.github.com>
2024-02-24 09:53:02 -0300

    precedencias

---
+++ Aulas/Java_introducao/src/Prioridades_op.java
@@ -0,0 +1,11 @@
+public class Prioridades_op {
+    public static void main(String[] args) {
+        //precendencia de sinais
+        //divisao
+        //multiplicação
+        //resto da divisao
+        //adição e subtração
+        int x = 10/2*3+4*3;
+        System.out.println(x);
+    }
+}
