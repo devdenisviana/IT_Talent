c8b9a088612f6315a7a459f8fbe843702c7c3a3c
Denis Jeronimo Viana Filho <41395264+devdenisviana@users.noreply.github.com>
2024-02-24 18:39:39 -0300

    conversao de dados

---
+++ Aulas/Java_introducao/src/ConversaoDeDados.java
@@ -0,0 +1,19 @@
+public class ConversaoDeDados {
+    public static void main(String[] args) {
+        String str = "100";
+            System.out.println(Integer.valueOf(str));
+            System.out.println(Byte.valueOf(str));
+            System.out.println(Short.valueOf(str));
+            System.out.println(Float.valueOf(str));
+            System.out.println(Double.valueOf(str));
+            System.out.println("-------------------------------");
+        Byte b = 10;
+            System.out.println(b.intValue());
+            System.out.println(b.shortValue());
+            System.out.println(b.floatValue());
+            System.out.println(b.doubleValue());
+            System.out.println(b.toString());
+        System.out.println("-------------------------------");
+
+    }
+}
